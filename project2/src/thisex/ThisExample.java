508a41a32fa316f14c89dd9248d54a044c5c380c
Chaerim Park <dasapcr@gmail.com>
2021-12-15 22:19:08 +0900

    upload

--- project2/src/thisex/ThisExample.java
+++ project2/src/thisex/ThisExample.java
@@ -1,5 +1,17 @@
 package thisex;
 
+class Birthday{
+	int day;
+	int month;
+	int year;
+	
+	public void setYear(int year) {
+		this.year = year; //지역변수를 멤버변수에 대입
+	}
+	
+	
+}
+
 public class ThisExample {
 
 }

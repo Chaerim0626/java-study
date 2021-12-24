64716c7e7b1f32b9de1abf967b5bb55186edd1d3
Chaerim Park <dasapcr@gmail.com>
2021-12-24 17:11:41 +0900

    1224 upload

--- project4_배열과ArrayList/src/project4_배열과ArrayList/ArrayListTest.java
+++ project4_배열과ArrayList/src/project4_배열과ArrayList/ArrayListTest.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 public class ArrayListTest {
 
 	public static void main(String[] args) {
+		
+		//ArrayList 클래스 : 자바에서 제공되는 객체 배열이 구현된 클래스
 		ArrayList<String> list = new ArrayList<String>();
 	}
 

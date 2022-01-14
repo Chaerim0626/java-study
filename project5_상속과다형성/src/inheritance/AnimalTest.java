a8383aec7e91b8e492d0fcef912245fd98dbf1ca
Chaerim Park <dasapcr@gmail.com>
2022-01-14 20:40:24 +0900

    0114 upload3

--- project5_상속과다형성/src/inheritance/AnimalTest.java
+++ project5_상속과다형성/src/inheritance/AnimalTest.java
@@ -1,12 +1,45 @@
 package inheritance;
 
+
+class Animal{
+	public void move() {
+		System.out.println("동물이 움직입니다.");
+	}
+}
+
+class Human extends Animal {
+	public void move() {
+		System.out.println("사람이 두발로 걷습니다.");
+	}
+}
+
+class Tiger extends Animal {
+	public void move() {
+		System.out.println("호랑이가 네발로 뜁니다.");
+	}
+}
+
+class Eagel extends Animal {
+	public void move() {
+		System.out.println("독수리가 하늘을 날읍니다.");
+	}
+}
+
+
 public class AnimalTest {
 
 	public static void main(String[] args) {
 		
+		AnimalTest test = new AnimalTest();
+		test.moveAnimal(new Human());
+		test.moveAnimal(new Tiger());
+		test.moveAnimal(new Eagel());
 		
-		
-
+	
+	}
+	
+	public void moveAnimal(Animal animal ) {
+		animal.move();
 	}
 
 }

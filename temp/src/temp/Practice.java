de36c24383c0b1c7ad9f8c5c4587c109d3cc0478
Chaerim Park <dasapcr@gmail.com>
2022-01-19 16:15:47 +0900

    0119 upload

---
+++ temp/src/temp/Practice.java
@@ -0,0 +1,35 @@
+package temp;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println(Byte.BYTES); //바이트 계산
+		System.out.println(Byte.SIZE); //비트 계산
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.MIN_VALUE);
+		
+		System.out.println(Short.BYTES); //바이트 계산
+		System.out.println(Short.SIZE); //비트 계산
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(Short.MIN_VALUE);
+		
+		System.out.println(Integer.BYTES); //바이트 계산
+		System.out.println(Integer.SIZE); //비트 계산
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		
+		System.out.println(Long.BYTES); //바이트 계산
+		System.out.println(Long.SIZE); //비트 계산
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Long.MIN_VALUE);
+		
+		System.out.println(Character.BYTES); //바이트 계산
+		System.out.println(Character.SIZE); //비트 계산
+		System.out.println((int)Character.MAX_VALUE);
+		System.out.println((int)Character.MIN_VALUE);
+		
+		
+	}
+}

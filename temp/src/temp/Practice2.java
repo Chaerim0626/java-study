15a3649aadee83343157f7d6b476274f0224a036
Chaerim Park <dasapcr@gmail.com>
2022-01-20 19:58:37 +0900

    0120 upload

--- temp/src/temp/Practice2.java
+++ temp/src/temp/Practice2.java
@@ -9,5 +9,17 @@ public class Practice2 {
 
 		byte b1 = 10;
 		System.out.printf("10은 8진수로 %o, 16진수로는 %x입니다.", b1,b1);
+	
+		System.out.println();
+		
+		//정수형 문자열로 변환해 정수의 자릿수 구하기
+		int a = 12345;
+		String str = "12345";
+		
+		String stra = String.valueOf(a); //정수 -> 문자열로 변환
+		System.out.println(stra.length()); //정수 자릿수
+	
+		int b2 = Integer.valueOf(str); //문자열 -> 정수로 변환
+		System.out.println(b2+1);
 	}
 }

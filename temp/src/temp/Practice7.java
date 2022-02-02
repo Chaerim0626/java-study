8dc7ef4bc24b4a40c84a8fd00bd24372238920d4
Chaerim Park <dasapcr@gmail.com>
2022-02-02 20:29:24 +0900

    0202 upload

---
+++ temp/src/temp/Practice7.java
@@ -0,0 +1,24 @@
+package temp;
+
+import java.util.*;
+//10950
+
+public class Practice7 {
+	public static void main(String[] args) {
+		
+	   Scanner sc = new Scanner(System.in);
+       int input = sc.nextInt();
+      
+       
+       for(int i=1; i<= input; i++){
+ 
+               int x = sc.nextInt();
+               int y = sc.nextInt();
+               
+               System.out.println("Case #" + i + ": " + x + " + " + y + " = " + (x+y));
+       }
+       
+
+       
+	}
+}

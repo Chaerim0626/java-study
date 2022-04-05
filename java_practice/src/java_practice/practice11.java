fa9910c29475fea9bb1c858abb19ba7ec23f8eaf
Chaerim Park <dasapcr@gmail.com>
2022-04-05 15:23:25 +0900

    0405 upload

--- java_practice/src/java_practice/practice11.java
+++ java_practice/src/java_practice/practice11.java
@@ -10,6 +10,17 @@ class Person2{
 	//getter, setter를 통해 멤버 필드의 값을 필요에 따라 사용
 	
 	//Constructor
+	Person2() {
+		
+	}
+	Person2(String name, int age, int height, int weight){
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		
+	}
+	
 	
 	//Method
 	public String getName() {
@@ -28,10 +39,69 @@ class Person2{
 	public void setWeight(int weight) { this.weight = weight;}
 	public int getWeight() {return weight;}
 	
+	public void move() {
+		System.out.println("이동중...");
+	}
+	
 }
 
 class Villain2 extends Person2{
+	//Field
+	private String unique_key;
+	private int weapon;
+	private double power;
 	
+	//Consturctor
+	Villain2() {}
+	Villain2(String name, int age, int height, int weight, String unique_key,int weapon, double power){
+		super(name, age, height, weight);
+		this.unique_key = unique_key;
+		this.weapon = weapon;
+		this.power = power;
+		
+	}
+	
+	
+	//Method
+	public void setUnique_key(String unique_key) {this.unique_key = unique_key;}
+	public String getUnique_key() {return unique_key;}
+	
+	public void setWeapon(int weapon) {this.weapon = weapon;}
+	public int getWeapon() {return weapon;}
+	
+	public void setPower(double power) {this.power = power;}
+	public double getPower() {return power;}
+	
+	public void printPerson() {
+		System.out.println("-----------------------");
+		System.out.println("악당 이름 : " + getName());
+		System.out.println("악당 나이 : " + getAge());
+		System.out.println("악당 키 : " + getHeight());
+		System.out.println("악당 체중 : " + getWeight());
+		System.out.println("악당 넘버 : " + getUnique_key());
+		System.out.println("악당 무기 : " + getWeaponName(getWeapon()));
+		System.out.println("악당 파워 : " + getPower());
+		System.out.println("-----------------------");
+	}
+	
+	public String getWeaponName(int a) {
+		String weapon;
+		switch(a) {
+			case 1:
+			weapon="창";
+			break;
+			case 2:
+				weapon="방패";
+				break;
+			case 3:
+				weapon="총";
+				break;
+			default:
+				weapon="---";
+				break;
+		}
+		return weapon;
+	}
 }
 
 class Hero2 extends Person2{
@@ -44,10 +114,10 @@ public class practice11 {
 	public static void main(String[] args) {
 		
 		//1. 객체 생성
-		Person2 p1 = new Person2();
-		
-		p1.setName("홍길동");
-		System.out.println(p1.getName());
+		Villain2 v1 = new Villain2("좀비", 20, 180, 80, "15001231", 1, 99.5);
+		v1.printPerson();
+		System.out.print(v1.getName() + "");
+		v1.move();
 		
 	}
 
